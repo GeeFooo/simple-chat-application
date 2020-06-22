529efe4737065271a32a38c15f5f5bcb283a9085
Gee Foo <nggheefook@gmail.com>
2020-06-22 18:40:12 +0800

    First commit

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+import time
+
+s = socket.socket()
+host = input(str("Please enter the IP of the server : "))
+port = 8080
+
+s.connect((host,port))
+
+print("Connected to chat server")
+
+while 1 :
+
+	incoming_message = s.recv(1024)
+	incoming_message = incoming_message.decode()
+	print("[Message Received] Server : " , incoming_message)
+	print("")
+	message = input(str(" > "))
+	message = message.encode()
+	s.send(message)
+	print("[Message Sended]")
+	print("")
